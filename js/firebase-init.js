06aefaf1145b447a78c3d1f6ab0d3654704516ec
plesenamdua2705 <plesenamdua2705@gmail.com>
2025-09-26 09:47:45 +0800

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,15 +1,16 @@
 // /js/firebase-init.js
-// Gunakan modular SDK (CDN)
+// Gunakan Modular SDK (CDN)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getAuth, onAuthStateChanged, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import { getAuth /* setPersistence dipindah ke halaman login */ } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
-// --- MASUKKAN CONFIG DARI FIREBASE CONSOLE ---
+// -- PASTIKAN CONFIG PERSIS DARI FIREBASE CONSOLE --
 const firebaseConfig = {
   apiKey: "AIzaSyAyC23ke5IEIoz5FpS41OiT1wyBSKaRDVk",
   authDomain: "e-stock-ff767.firebaseapp.com",
   projectId: "e-stock-ff767",
-  storageBucket: "e-stock-ff767.firebasestorage.app",
+  // Disarankan verifikasi storageBucket dari Console (biasanya .appspot.com):
+  storageBucket: "e-stock-ff767.appspot.com",
   messagingSenderId: "158396302579",
   appId: "1:158396302579:web:1626da0203660ce9a3a386"
 };
@@ -17,9 +18,6 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Persistensi login
-await setPersistence(auth, browserLocalPersistence);
 
 // Util: pastikan dokumen user ada
 export async function ensureUserDoc(user) {
@@ -30,7 +28,7 @@ export async function ensureUserDoc(user) {
     await setDoc(ref, {
       email: user.email ?? "",
       displayName: user.displayName ?? "",
-      role: "viewer",   // default role
+      role: "viewer",       // default role
       disabled: false,
       createdAt: serverTimestamp(),
       updatedAt: serverTimestamp()
@@ -38,10 +36,6 @@ export async function ensureUserDoc(user) {
   }
 }
 
-// Listener global (opsional, untuk debugging)
-onAuthStateChanged(auth, async (user) => {
-  if (user) await ensureUserDoc(user);
-});
-
-
-
+// (Opsional) listener untuk debugging
+// import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+// onAuthStateChanged(auth, (user) => { console.log('Auth:', !!user, user?.uid); });
