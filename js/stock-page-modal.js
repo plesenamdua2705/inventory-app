8ce42bdd80a36e1f749a00a67f11f6f546aaf8c0
plesenamdua2705 <plesenamdua2705@gmail.com>
2025-09-26 16:09:21 +0800

    Update stock-page-modal.js

--- js/stock-page-modal.js
+++ js/stock-page-modal.js
@@ -1,4 +1,4 @@
-<!-- /js/stock-page-modal.js -->
+// /js/stock-page-modal.js //
 <script type="module">
 import { auth, db } from "./firebase-init.js";
 import {
@@ -291,3 +291,4 @@ export function initStockPageModal(cfg) {
   });
 }
 </script>
+
